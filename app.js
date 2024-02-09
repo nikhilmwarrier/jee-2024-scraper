462fc35a4dacacd82a44d56cebd24bb68a7325fb
MeninGaming <69890484+MeninGaming@users.noreply.github.com>
2024-02-09 12:24:14 +0530

    added subject specific marks

--- app.js
+++ app.js
@@ -5,11 +5,11 @@ form.addEventListener("submit", async e => {
     const shift = formData.get("shift");
     const fetchedResponse = await fetch("./keys/" + shift + ".json");
     const ntaAnswers = await fetchedResponse.json();
-    console.log(1);
-    console.log(ntaAnswers);
+    // console.log(1);
+    // console.log(ntaAnswers);
     try {
         const responses = JSON.parse(formData.get("responses"));
-        console.log(responses);
+        // console.log(responses);
         compareAnswers(ntaAnswers, responses, shift);
     } catch (err) {
         console.error("JSONError:", err);
@@ -18,23 +18,38 @@ form.addEventListener("submit", async e => {
 });
 
 function compareAnswers(nta, user, shift) {
-    let correct = 0;
-    let incorrect = 0;
-    let skipped = 0;
+    let overallCorrect = 0;
+    let overallIncorrect = 0;
+    let overallSkipped = 0;
     let errorInKeys = [];
+    let correct = {
+        0: 0, // 0 = Maths
+        1: 0, // 1 = Physics
+        2: 0, // 2 = Chem
+    }
+    let incorrect = {
+        0: 0, // 0 = Maths
+        1: 0, // 1 = Physics
+        2: 0, // 2 = Chem
+    }
+
+    const firstKey = Object.keys(nta).sort((a, b) => a - b)[0]
     for (const key in nta) {
         const ntaAns = Number(`${nta[key]}`.trim());
+        const subject = Math.floor((key % firstKey) / 30);
+        console.log(subject)
         if (user.hasOwnProperty(key)) {
             if (user[key]['hasAnswered']) {
                 const ownAns = Number(`${user[key].ownAnswer}`.trim());
                 if (ntaAns === ownAns) {
-                    correct += 1;
+                    overallCorrect += 1;
+                    correct[subject] += 1;
                 } else {
-                    incorrect += 1;
-                    // console.log(nta[key], user[key].ownAnswer);
+                    overallIncorrect += 1;
+                    incorrect[subject] += 1;
                 }
             } else {
-                skipped += 1;
+                overallSkipped += 1;
             }
         } else errorInKeys.push(key);
     }
@@ -42,23 +57,36 @@ function compareAnswers(nta, user, shift) {
         alert("Error in keys:\n" + errorInKeys.toString());
         errorInKeys = [];
     }
-    generateScorecard(correct, incorrect, shift);
+    generateScorecard(overallCorrect, overallIncorrect, shift, [correct, incorrect]);
 }
 
-function generateScorecard(correct, incorrect, shift) {
-    console.log(1);
+function generateScorecard(overallCorrect, overallIncorrect, shift, allData) {
+    // console.log(1);
     const resultDiv = document.getElementById("result");
     const shiftEl = resultDiv.querySelector(".shift span");
     const scoreEl = resultDiv.querySelector(".score span");
     const attemptedEl = resultDiv.querySelector("table .attempted");
     const correctEl = resultDiv.querySelector("table .correct");
     const incorrectEl = resultDiv.querySelector("table .incorrect");
+    const tableScoreEl = resultDiv.querySelector("table .score")
+    const subjects = {
+        "maths": 0,
+        "phy": 1,
+        "chem": 2
+    }
+    for (const key in subjects) {
+        resultDiv.querySelector(`table .attempted-${key}`).innerText = allData[0][subjects[key]] + allData[1][subjects[key]]
+        resultDiv.querySelector(`table .correct-${key}`).innerText = allData[0][subjects[key]]
+        resultDiv.querySelector(`table .incorrect-${key}`).innerText = allData[1][subjects[key]]
+        resultDiv.querySelector(`table .score-${key}`).innerText = calculateScore(allData[0][subjects[key]], allData[1][subjects[key]])
+    }
 
     shiftEl.innerText = shift;
-    scoreEl.innerText = calculateScore(correct, incorrect);
-    attemptedEl.innerText = correct + incorrect;
-    correctEl.innerText = correct;
-    incorrectEl.innerText = incorrect;
+    scoreEl.innerText = calculateScore(overallCorrect, overallIncorrect);
+    attemptedEl.innerText = overallCorrect + overallIncorrect;
+    correctEl.innerText = overallCorrect;
+    incorrectEl.innerText = overallIncorrect;
+    tableScoreEl.innerText = calculateScore(overallCorrect, overallIncorrect);
 }
 
 function calculateScore(correct, incorrect) {
